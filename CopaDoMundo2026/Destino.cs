3179afde5a64000b7575ab6692fadf6590438fef
AndreHigashi31 <a.higashi31@gmail.com>
2022-12-23 20:07:19 -0300

    atualizacao

--- CopaDoMundo2026/Destino.cs
+++ CopaDoMundo2026/Destino.cs
@@ -11,15 +11,15 @@ namespace CopaDoMundo2026
     public class Destino
 
     {
-        public string destinoViagem { get; set; }
+        public  string destinoViagem { get; set; }
         public int diasViajando { get; set; }
         public string tipoIngresso { get; set; }
         public string acompanhantes { get; set; }
-        public int quantidadeDeAcompanhantes { get; set; }
-        public double alimentacao{ get; set; }
+        public int quantidadeAcompanhantes { get; set; }
+        public double alimentacao = 75;
         public double passagem { get; set; }
         public double hospedagem { get; set; }
-
+        public double dolar = 5.20;
+        public double gastosTotais { get; set; }
     }
-    
 }
--- CopaDoMundo2026/Program.cs
+++ CopaDoMundo2026/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CopaDoMundo2026;
 using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 
 Console.WriteLine("Rumo a Copa do Mundo 2026!");
@@ -27,11 +28,146 @@ if(rumoACopa.acompanhantes  == "n")
 {
     Console.WriteLine("Quantas pessoas vão viajar com você?");
     int pessoasAdicionais = Int32.Parse(Console.ReadLine());
-    rumoACopa.quantidadeDeAcompanhantes = pessoasAdicionais;
+    rumoACopa.quantidadeAcompanhantes = pessoasAdicionais;
 }
 Console.WriteLine($"Quais jogos você quer assister em {rumoACopa.destinoViagem}?");
 Console.WriteLine(" [1] - Abertura: \r\n [2] - Fase de Grupos; \r\n [3] - Oitavas de Final; \r\n [4] - Quartas de Final; \r\n [5] - Semi Final; \r\n [6] - FINAL!");
 rumoACopa.tipoIngresso = Console.ReadLine();
+
+
+switch (rumoACopa.destinoViagem)
+{
+    case "Vancouver":
+        rumoACopa.passagem = 7000;
+        rumoACopa.hospedagem = 400 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Toronto":
+        rumoACopa.passagem = 5000;
+        rumoACopa.hospedagem = 500 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Atlanta":
+        rumoACopa.passagem = 7000;
+        rumoACopa.hospedagem = 500 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Boston":
+        rumoACopa.passagem = 4500;
+        rumoACopa.hospedagem = 600 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+        
+
+    case "Dallas":
+        rumoACopa.passagem = 7000;
+        rumoACopa.hospedagem = 450 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+      
+
+    case "Filadelfia":
+        rumoACopa.passagem = 6500;
+        rumoACopa.hospedagem = 400 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+       
+
+    case "Houston":
+        rumoACopa.passagem = 6000;
+        rumoACopa.hospedagem = 400 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+       
+
+    case "Kansas City":
+        rumoACopa.passagem = 6500;
+        rumoACopa.hospedagem = 500 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break; ;
+        
+
+    case "Los Angeles":
+        rumoACopa.passagem = 4000;
+        rumoACopa.hospedagem = 400 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+        
+
+    case "Miami":
+        rumoACopa.passagem = 4000;
+        rumoACopa.hospedagem = 400 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Nova Iorque":
+        rumoACopa.passagem = 4000;
+        rumoACopa.hospedagem = 600 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "São Francisco":
+        rumoACopa.passagem = 4500;
+        rumoACopa.hospedagem = 350 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Seattle":
+        rumoACopa.passagem = 7000;
+        rumoACopa.hospedagem = 500 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Cidade do México":
+        rumoACopa.passagem = 3800;
+        rumoACopa.hospedagem = 300 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Guadalajara":
+        rumoACopa.passagem = 5500;
+        rumoACopa.hospedagem = 250 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+    case "Monterrey":
+        rumoACopa.passagem = 5000;
+        rumoACopa.hospedagem = 250 * rumoACopa.diasViajando;
+        rumoACopa.alimentacao = (rumoACopa.alimentacao * rumoACopa.dolar) * rumoACopa.diasViajando;
+        rumoACopa.gastosTotais = rumoACopa.hospedagem + ((rumoACopa.passagem + rumoACopa.alimentacao) * (rumoACopa.quantidadeAcompanhantes + 1));
+        break;
+
+
+    default:
+        Console.WriteLine("Você escolheu uma cidade que não será sede da Copa 2026, escolha novamente!");
+        break;
+
+ 
+}
+Console.WriteLine($"Com passagens area para {rumoACopa.destinoViagem}, vamos gastar R$ {rumoACopa.passagem}");
+Console.WriteLine($"Com hospedagem vamos gastar R$ {rumoACopa.hospedagem}");
+Console.WriteLine($"Com alimentação vamos gastar R$ {rumoACopa.alimentacao}");
+Console.WriteLine($"Ao todo nossa viagem sairá por R$ {rumoACopa.gastosTotais}");
+
+
 
 
 
